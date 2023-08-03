7384ef5f8787a628cb5beb2564a7a6851d3843b4
gabebraga <113937939+gabebraga@users.noreply.github.com>
2023-08-03 16:00:54 -0300

    modal kanban

--- js/kanban/modal-proj.js
+++ js/kanban/modal-proj.js
@@ -1,30 +1,21 @@
-const dropdowns = document.querySelectorAll('.dropdown-home-kanban');
-
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select-home-kanban');
-    const caret = dropdown.querySelector('.caret-home-kanban');
-    const menu = dropdown.querySelector('.menu-home-kanban');
-    const options = dropdown.querySelectorAll('.menu-home-kanban li');
-    const selected = dropdown.querySelector('.selected-home-kanban');
-
+  // Obtém os elementos do DOM
+  var modalkanban = document.getElementById("myModal-kanban");
+  var buttonkanban = document.getElementById("myButton-kanban");
+  var spankanban = document.getElementsByClassName("close-kanban")[0];
   
-select.addEventListener('click-kanban', () => {
-  select.classList.toggle('select-clicked-kanban');
-  caret.classList.toggle('caret-rotate-kanban');
-  menu.classList.toggle('menu-open-kanban');
-});
-
-options.forEach(option => {
-  option.addEventListener('click-kanban', () => {
-      selected.innerText = option.innerText;
-      select.classList.remove('select-clicked-kanban');
-      caret.classList.remove('caret-rotate-kanban');
-      menu.classList.remove('menu-open-kanban');
-    
-      options.forEach(option => {
-        option.classList.remove('active-home-kanban');
-        });
-          option.classList.add('active-home-kanban');
-  });
-});
-});
+  // Ação ao clicar no botão para abrir o modal
+  buttonkanban.onclick = function() {
+    modalkanban.style.display = "block";
+  };
+  
+  // Ação ao clicar no "x" para fechar o modal
+  spankanban.onclick = function() {
+    modalkanban.style.display = "none";
+  };
+  
+  // Ação ao clicar fora do modal para fechá-lo
+  window.onclick = function(event) {
+    if (event.target == modalkanban) {
+      modalkanban.style.display = "none";
+    }
+  };
