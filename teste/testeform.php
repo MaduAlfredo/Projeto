2d31e5ad7d04e07564d3f4034a33600d32a28e0e
Maria Eduarda Alfredo dos Santos <mariaedualfredo@gmail.com>
2023-08-09 14:15:57 -0300

    acaba

--- teste/testeform.php
+++ teste/testeform.php
@@ -1,13 +1,43 @@
-<form action="../teste/process.php" method="POST">
-    <input type="text" name="boardName" placeholder="Título do Board">
-    <textarea name="boardDesc" id="" cols="30" rows="10"></textarea>
-    <input type="date" name="boardTime" id="">
-    <div id="complex" class="complex">
-        <select name="complex" id="">
-            <option value="alta">Alta</option>
-            <option value="media">Média</option>
-            <option value="baixa">Baixa</option>
-        </select>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/kanban.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+    <link rel="stylesheet" href="../css/modal-kanban.css">
+</head>
+<body>
+<form action="../teste/process.php" method="POST" >
+    <div class="modal-content-kanban">
+        <h1 class="modal-title-kanban">Novo Projeto</h1>
+        <div class="modal-elements-kanban">
+            <h2>Nome do Projeto</h2>
+            <input type="text" name="boardName" placeholder="Título do Board">
+        </div>
+        <div class="modal-elements-kanban">
+            <h2>Descrição do Projeto:</h2>
+            <textarea name="boardDesc" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div class="modal-elements-kanban">
+            <h2>Expectativa de Conclusão:</h2>
+            <input type="date" name="boardTime" id="">
+        </div>
+        <!-- <div class="dropdown-kanban">
+        </div> -->
+
+            <div id="complex" class="complex" class="dropdown-kanban">
+                <select name="complex" class="menuKanban">
+                    <option value="alta">Alta</option>
+                    <option value="media">Média</option>
+                    <option value="baixa">Baixa</option>
+                </select>
+            </div>
+        <button class="modal-save">Cancelar</button>
+        <input class="modal-cancel" type="submit" value="Criar">
     </div>
-    <input type="submit" value="Enviar">
 </form>
+
+</body>
+</html>
