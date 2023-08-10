b21d4f15f391fb722bdce3329ade7c475e51685c
gabebraga <113937939+gabebraga@users.noreply.github.com>
2023-08-10 00:59:12 -0300

    deu boaaaa

--- teste/kanbanteste.php
+++ teste/kanbanteste.php
@@ -5,36 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        *{
-            background-color: black;
-            color: white;
+        .card{
+            width: 50%;
+            background-color: lavender;
         }
     </style>
     <title>Home</title>
 </head>
 <body>
-<img src="../fotos/mr.incredible.png">
+    <img src="../fotos/mr.incredible.png" alt="">
 <div>
 <?php
-include_once 'conexao.php';
+include_once '../php/conexao.php';
 
 if ($conexao->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
 }
 
-$sql = "SELECT id_dono, board_name, board_desc FROM projects";
+$sql = "SELECT * FROM projects";
 $result = $conexao->query($sql);
 
-var_dump($id_dono);
-var_dump($board_name);
-var_dump($board_desc);
-
+if (!$result) {
+    die("Erro na consulta SQL: " . $conexao->error);
+}
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<div class='card'>";
         echo "<h2>" . $row["board_name"] . "</h2>";
-        echo "<p>Descrição do board: $" . $row["board_desc"] . "</p>";
+        echo "<p>Id do board: " . $row["id_board"] . "</p>";
+        echo "<p>Descrição do board: " . $row["board_desc"] . "</p>";
         echo "</div>";
     }
 } else {
