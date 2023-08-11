391c01ab93347c9e888425e7671e51bb97bbbecd
Maria Eduarda <mariaedualfredo@gmail.com>
2023-08-11 14:32:02 -0300

    commit

--- html/kanban.php
+++ html/kanban.php
@@ -44,7 +44,32 @@
                         die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
                     }
 
-                    include_once '../php/userId.php';
+                    
+
+$email = $_SESSION["email"]; // Make sure to sanitize and validate the email
+
+$query = "SELECT id_user FROM users WHERE email = '$email'";
+$stmt = mysqli_prepare($conexao, $query);
+
+if ($stmt) {
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_bind_result($stmt, $userId);
+
+    if (mysqli_stmt_fetch($stmt)) {
+        // $userId now holds the fetched value
+        // Use $userId as needed
+    } else {
+        // Handle fetch error
+    }
+
+    mysqli_stmt_close($stmt);
+} else {
+    // Handle statement preparation error
+    echo "Statement Error: " . mysqli_error($conexao);
+}
+
+$_SESSION["userId"] = $userId;
 
                     // Consulta somente os boards do usuário específico
                     $sql = "SELECT * FROM projects WHERE id_dono = '$userId'";
